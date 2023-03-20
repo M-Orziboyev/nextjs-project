d60e9b5d43894bec6dae921ba430e4d7e3d3d96b
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2023-03-20 06:25:58 +0500

    Navbar

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image"
 
 function Header() {
     return (
-        <div>Header</div>
+        <header>
+            <div className="flex items-center space-x-2 md:space-x-10">
+                <Image src={"/logo.svg"} alt={"logo"} width={56} height={56} className="cursor-pointer object-contain"/>
+                <ul className="space-x-4 md:flex hidden">
+                    <li className="navLink">Home</li>
+                    <li className="navLink">Movies</li>
+                    <li className="navLink">TV Shows</li>
+                    <li className="navLink">New</li>
+                    <li className="navLink">Popular</li>
+                </ul>
+            </div>
+            <div>
+
+            </div>
+        </header>
     )
 }
 
