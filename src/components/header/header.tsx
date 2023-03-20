4293270f45813ae8a88139b91fe3db3a46d8a95d
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2023-03-20 07:17:50 +0500

    header components

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,29 @@
 import Image from "next/image"
+import { useEffect, useState } from "react"
+import { AiOutlineSearch, AiOutlineUser } from "react-icons/ai"
+import { BsBellSlash } from 'react-icons/bs'
 
 function Header() {
+    const [scrolled, setScrolled] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setScrolled(true)
+            } else {
+                setScrolled(false)
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [])
+
     return (
-        <header>
+        <header className={`${scrolled && 'bg-slate-500'}`}>
             <div className="flex items-center space-x-2 md:space-x-10">
-                <Image src={"/logo.svg"} alt={"logo"} width={56} height={56} className="cursor-pointer object-contain"/>
+                <Image src={"/logo.svg"} alt={"logo"} width={56} height={56} className="cursor-pointer object-contain" />
                 <ul className="space-x-4 md:flex hidden">
                     <li className="navLink">Home</li>
                     <li className="navLink">Movies</li>
@@ -13,8 +32,11 @@ function Header() {
                     <li className="navLink">Popular</li>
                 </ul>
             </div>
-            <div>
-
+            <div className="flex items-center space-x-4 text-sm font-light ">
+                <AiOutlineSearch className="h-6 w-6 fill-slate-50 cursor-pointer" />
+                <p className="hidden lg:inline text-white cursor-pointer">Kids</p>
+                <BsBellSlash className="h-6 w-6 fill-slate-50 cursor-pointer" />
+                <AiOutlineUser className="h-6 w-6 fill-slate-50 cursor-pointer" />
             </div>
         </header>
     )
