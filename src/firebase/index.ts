464da06ff7ac94781e1321a22a3e7fbfcfaae6c0
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2023-03-27 22:30:25 +0500

    test commit

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,4 +1,4 @@
-import { initializeApp, getApp, getApps } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth"
 
