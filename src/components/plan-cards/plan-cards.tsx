05312c3164af6b75ddf47b37cf59b7effb85c8bc
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2023-04-10 20:49:35 +0500

    Stripe

--- src/components/plan-cards/plan-cards.tsx
+++ src/components/plan-cards/plan-cards.tsx
@@ -1,7 +1,44 @@
+import React from 'react';
 import {PlanCardsProps} from "./plan-cards-props";
+import Image from "next/image";
+import {RiVipCrown2Line} from "react-icons/ri";
+import {AiOutlineHourglass, AiOutlineVideoCameraAdd} from "react-icons/ai";
 
-const PlanCard = ({}: PlanCardsProps) => {
-    return <div>PlanCard</div>
-}
+const PlanCards = ({}: PlanCardsProps) => {
+    return <div key={product.id} className="max-w-sm cursor-pointer bg-white/20 px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-5000">
+        <h3 className="mb-3 text-xl font-bold text-red-600">{product.name}</h3>
+        <div className="relative">
+            <Image
+                src={product.images[0]}
+                alt='Colors'
+                className='rounded-xl w-full object-cover'
+                width={400}
+                height={400}
+            />
+            <p className="absolute top-0 bg-black/70 text-white font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">${product.default_price.unit_amount}</p>
+            <div className='absolute rounded-xl left-0 right-0 bottom-0 top-0 bg-black/20 w-full h-full' />
+        </div>
+        <div className="border-[1px] border-white/20 mt-4" />
+        <button className="mt-4 bg-red-600 hover:opacity-75 w-full py-4 rounded font-semibold">BUY PLAN</button>
+        <div className="my-4 flex flex-col space-y-2">
+            {product.metadata.adv.split(', ').map((c, id) => (
+                <div key={c} className='flex space-x-2 items-center'>
+                    {id == 0 && <RiVipCrown2Line className={'w-5 h-5'}/>}
+                    {id == 1 && <AiOutlineHourglass className={'w-5 h-5'}/>}
+                    {id == 2 && <AiOutlineVideoCameraAdd className={'w-5 h-5'}/>}
+                    <p>{c}.</p>
+                </div>
+            ))}
+            {/* <div className="flex space-x-2 items-center">
+                                    <AiOutlineHourglass className="w-5 h-5" />
+                                    <p>100 hour video.</p>
+                                </div>
+                                <div className="flex space-x-2 items-center">
+                                    <AiOutlineVideoCameraAdd className="w-5 h-5" />
+                                    <p>HD format</p>
+                                </div> */}
+        </div>
+    </div>
+};
 
-export default  PlanCard
+export default PlanCards;
--- src/components/subscription-plan/subscription-plan.tsx
+++ src/components/subscription-plan/subscription-plan.tsx
@@ -3,6 +3,7 @@ import useAuth from '../../hooks/useAuth';
 import { RiVipCrown2Line } from 'react-icons/ri'
 import { AiOutlineHourglass, AiOutlineVideoCameraAdd } from "react-icons/ai";
 import { SubscriptionPlanProps } from "./subscription-plan-props";
+import PlanCards from "../plan-cards/plan-cards";
 
 const SubscriptionPlan = ({ products }: SubscriptionPlanProps) => {
     
@@ -19,42 +20,7 @@ const SubscriptionPlan = ({ products }: SubscriptionPlanProps) => {
             </div>
             <div className=" flex justify-center items-center py-20">
                 <div className="md:px-4 md:grid md:grid-cols-2 lg:grid-cols-3 gap-5 space-y-4 md:space-y-0">
-                    {products.map(product => (
-                        <div key={product.id} className="max-w-sm cursor-pointer bg-white/20 px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-5000">
-                            <h3 className="mb-3 text-xl font-bold text-red-600">{product.name}</h3>
-                            <div className="relative">
-                                <Image
-                                    src={product.images[0]}
-                                    alt='Colors'
-                                    className='rounded-xl w-full object-cover'
-                                    width={400}
-                                    height={400}
-                                />
-                                <p className="absolute top-0 bg-black/70 text-white font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">${product.default_price.unit_amount}</p>
-                                <div className='absolute rounded-xl left-0 right-0 bottom-0 top-0 bg-black/20 w-full h-full' />
-                            </div>
-                            <div className="border-[1px] border-white/20 mt-4" />
-                            <button className="mt-4 bg-red-600 hover:opacity-75 w-full py-4 rounded font-semibold">BUY PLAN</button>
-                            <div className="my-4 flex flex-col space-y-2">
-                                {product.metadata.adv.split(', ').map((c, id) => (
-                                    <div key={c} className='flex space-x-2 items-center'>
-                                        {id == 0 && <RiVipCrown2Line className={'w-5 h-5'}/>}
-                                        {id == 1 && <AiOutlineHourglass className={'w-5 h-5'}/>}
-                                        {id == 2 && <AiOutlineVideoCameraAdd className={'w-5 h-5'}/>}
-                                        <p>{c}.</p>
-                                    </div>
-                                ))}
-                                {/* <div className="flex space-x-2 items-center">
-                                    <AiOutlineHourglass className="w-5 h-5" />
-                                    <p>100 hour video.</p>
-                                </div>
-                                <div className="flex space-x-2 items-center">
-                                    <AiOutlineVideoCameraAdd className="w-5 h-5" />
-                                    <p>HD format</p>
-                                </div> */}
-                            </div>
-                        </div>
-                    )).reverse()}
+                    {products.map(product => <PlanCards key={product.id} product={product} />).reverse()}
                 </div>
             </div>
         </div>
