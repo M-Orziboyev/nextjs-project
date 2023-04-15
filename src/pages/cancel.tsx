655a9453afda180ad58996867e754c4f445a3e17
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2023-04-15 16:57:57 +0500

    Cancel page

--- src/pages/cancel.tsx
+++ src/pages/cancel.tsx
@@ -12,7 +12,7 @@ function Success() {
             </div>
             <div className="h-[90vh] flex flex-col justify-center items-center">
                 <BiErrorAlt className="w-20 h-20 text-red-500" />
-                <h1 className="text-2xl md:text-5xl mt-5">Canceled Plan</h1>
+                <h1 className="text-2xl  md:text-5xl mt-5">Canceled Plan</h1>
                 <Link href='/'>
                     <button className="mt-4 bg-[#E10856] rounded py-4 px-7">Choose Plan</button>
                 </Link>
